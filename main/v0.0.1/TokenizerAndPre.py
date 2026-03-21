d563eadb14813eae0f5cafb737acbbe0c56c408c
up-th-bilibili <1710318399@qq.com>
2026-03-21 11:46:17 +0800

    Update TokenizerAndPre.py

--- main/v0.0.1/TokenizerAndPre.py
+++ main/v0.0.1/TokenizerAndPre.py
@@ -1,4 +1,12 @@
 from BaseTokenizer import *
 class Tokenizer(BaseTokenizer):
-    def __init__(self):
+    def match_switch_line(self):
         pass
+    def __init__(self,text):
+        mfuncs = (
+            self.match_switch_line,
+            self.jump_space,
+            self.match_keyword,
+            self.match_name
+        )
+        super().__init__(text,mfuncs)
